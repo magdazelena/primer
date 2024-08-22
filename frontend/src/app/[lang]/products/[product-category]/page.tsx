43d5afb8b74b1d03a2ff8268077c81bdf77d2f62
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2024-08-22 11:42:40 +0200

    chore: extracted child category logic

--- frontend/src/app/[lang]/products/[product-category]/page.tsx
+++ frontend/src/app/[lang]/products/[product-category]/page.tsx
@@ -1,34 +1,6 @@
 import PageHeader from "@/app/[lang]/components/PageHeader";
-import { fetchAPI } from "@/app/[lang]/utils/fetch-api";
 import List from "@/app/[lang]/views/product-list";
-import { ProductCategory } from "../../../../types/product";
-
-async function fetchPostsByCategory(filter: string) {
-  const parentCategory = await fetchAllChildCategories(filter);
-  try {
-    const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-    const path = `/products`;
-    const urlParamsObject = {
-      sort: { createdAt: "desc" },
-      filters: {
-        category: {
-          slug: { $in: parentCategory?.childrenCategories },
-        },
-      },
-      populate: {
-        coverImage: { fields: ["url"] },
-        category: {
-          populate: "*",
-        },
-      },
-    };
-    const options = { headers: { Authorization: `Bearer ${token}` } };
-    const responseData = await fetchAPI(path, urlParamsObject, options);
-    return { category: parentCategory?.parent, posts: responseData };
-  } catch (error) {
-    console.error(error);
-  }
-}
+import { fetchPostsByCategory } from "@/app/[lang]/utils/categories-fetch";
 
 export default async function ProductCategoryRoute({
   params,
@@ -36,7 +8,11 @@ export default async function ProductCategoryRoute({
   params: { "product-category": string };
 }) {
   const filter = params["product-category"];
-  const data = await fetchPostsByCategory(filter);
+  const data = await fetchPostsByCategory(
+    "/products",
+    "/product-categories",
+    filter
+  );
 
   //TODO: CREATE A COMPONENT FOR THIS
   if (!data || data.posts.data.length === 0)
@@ -57,43 +33,3 @@ export default async function ProductCategoryRoute({
 export async function generateStaticParams() {
   return [];
 }
-
-async function fetchAllChildCategories(slug: string) {
-  try {
-    const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-    const path = `/product-categories`;
-    const params = {
-      filters: { slug },
-      populate: {
-        children: {
-          populate: "children", // Recursively populate children categories
-        },
-      },
-    };
-
-    const options = { headers: { Authorization: `Bearer ${token}` } };
-    const responseData = await fetchAPI(path, params, options);
-    const category = responseData.data[0];
-    const childrenCategories = collectAllSlugs(category);
-    return {
-      parent: category,
-      childrenCategories,
-    };
-  } catch (error) {
-    console.error(error);
-  }
-}
-const collectAllSlugs = (category: ProductCategory) => {
-  const slugs = [category.attributes.slug];
-
-  if (
-    category.attributes.children &&
-    category.attributes.children.data.length > 0
-  ) {
-    category.attributes.children.data.forEach((child) => {
-      slugs.push(...collectAllSlugs(child));
-    });
-  }
-
-  return slugs;
-};
---
+++ frontend/src/app/[lang]/utils/categories-fetch.ts
@@ -0,0 +1,74 @@
+import { ProductCategory } from "@/types/product";
+import { fetchAPI } from "./fetch-api";
+
+async function fetchAllChildCategories(path: string, slug: string) {
+  try {
+    const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+    const params = {
+      filters: { slug },
+      populate: {
+        children: {
+          populate: "children", // Recursively populate children categories
+        },
+      },
+    };
+
+    const options = { headers: { Authorization: `Bearer ${token}` } };
+    const responseData = await fetchAPI(path, params, options);
+    const category = responseData.data[0];
+    const childrenCategories = collectAllSlugs(category);
+    return {
+      parent: category,
+      childrenCategories,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+const collectAllSlugs = (category: ProductCategory) => {
+  const slugs = [category.attributes.slug];
+
+  if (
+    category.attributes.children &&
+    category.attributes.children.data.length > 0
+  ) {
+    category.attributes.children.data.forEach((child) => {
+      slugs.push(...collectAllSlugs(child));
+    });
+  }
+
+  return slugs;
+};
+export async function fetchPostsByCategory(
+  path: string,
+  categoryPath: string,
+  filter: string
+) {
+  const parentCategory = await fetchAllChildCategories(categoryPath, filter);
+  try {
+    const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+    const urlParamsObject = {
+      sort: { createdAt: "desc" },
+      filters: {
+        category: {
+          slug: { $in: parentCategory?.childrenCategories },
+        },
+      },
+      populate: {
+        coverImage: { fields: ["url"] },
+        cover: { fields: ["url"] },
+        category: {
+          populate: "*",
+        },
+        authorsBio: {
+          populate: "*",
+        },
+      },
+    };
+    const options = { headers: { Authorization: `Bearer ${token}` } };
+    const responseData = await fetchAPI(path, urlParamsObject, options);
+    return { category: parentCategory?.parent, posts: responseData };
+  } catch (error) {
+    console.error(error);
+  }
+}
