6a093b7a2aa27486cf8c2c01cd8d9d28584bc82e
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2024-09-30 18:32:39 +0200

    feat: styles for desktop menu

--- frontend/src/app/[lang]/components/Navbar/DesktopNav/DesktopDropdown.tsx
+++ frontend/src/app/[lang]/components/Navbar/DesktopNav/DesktopDropdown.tsx
@@ -2,31 +2,27 @@ import { Category } from "@/types/article";
 import { ProductCategory } from "@/types/product";
 import { NavLink } from "@/app/[lang]/components/NavLink";
 
-interface DesktopDropdown {
+interface DesktopDropdownProps {
   title: string;
   categories: Array<Category | ProductCategory>;
   basePath: string;
-  activeMenu: string;
-  onSetActiveMenu: (menu: string) => void;
 }
 
 const DesktopDropdown = ({
   title,
   categories,
   basePath,
-  activeMenu,
-  onSetActiveMenu,
-}: DesktopDropdown) => {
+}: DesktopDropdownProps) => {
   const renderMenu = (category: Category | ProductCategory) => {
     return (
-      <li key={category.id} className="ml-4">
+      <li key={category.id} className="relative group">
         <NavLink
           text={category.attributes.name}
-          url={category.attributes.slug}
-          className="block py-1 hover:underline"
+          url={`${basePath}/${category.attributes.slug}`}
+          className="block px-4 py-2 hover:bg-gray-100"
         />
         {category.attributes.children?.data.length ? (
-          <ul className="ml-4">
+          <ul className="relative w-full hidden group-hover:block">
             {category.attributes.children.data.map(
               (childCategory: Category | ProductCategory) =>
                 renderMenu(childCategory)
@@ -36,17 +32,22 @@ const DesktopDropdown = ({
       </li>
     );
   };
+
   return (
     <li className="relative group">
-      <NavLink url={basePath} text={title} />
-      <div className="fixed left-0 w-full hidden group-hover:block bg-white shadow-lg">
-        <ul className="p-4">
-          {categories.map((category: Category | ProductCategory) => {
-            if (category.attributes.topLevel) return renderMenu(category);
-            return null;
-          })}
+      <NavLink
+        url={basePath}
+        text={title}
+        className="block px-4 py-2 hover:bg-gray-200"
+      />
+      {categories.length > 0 && (
+        <ul className="fixed left-0 p-5 w-full hidden group-hover:flex top-level bg-white shadow-lg">
+          <span className="block h-[2px] w-full bg-secondary mb-5"></span>
+          {categories.map((category: Category | ProductCategory) =>
+            renderMenu(category)
+          )}
         </ul>
-      </div>
+      )}
     </li>
   );
 };
--- frontend/src/app/[lang]/components/Navbar/DesktopNav/index.tsx
+++ frontend/src/app/[lang]/components/Navbar/DesktopNav/index.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { useState } from "react";
 import { NavLink } from "@/app/[lang]/components/NavLink";
-
 import DesktopDropdown from "./DesktopDropdown";
 
 const DesktopNav = ({
@@ -9,26 +7,29 @@ const DesktopNav = ({
   categories,
 }: {
   links: Array<NavLink>;
-
   categories: { productCategories: Array<any>; blogCategories: Array<any> };
 }) => {
-  const [activeMenu, setActiveMenu] = useState("");
-
+  const renderProductTopLevelCategories = () => {
+    return categories.productCategories.map((category: any) => {
+      if (category.attributes.topLevel) {
+        return (
+          <DesktopDropdown
+            key={category.id}
+            title={category.attributes.name}
+            categories={category.attributes.children?.data || []}
+            basePath={category.attributes.slug}
+          />
+        );
+      }
+      return null;
+    });
+  };
   return (
     <div className="items-center hidden lg:flex">
       <ul className="items-stretch hidden space-x-3 lg:flex">
         {links.map((item: NavLink) => {
           if (item.url === "/products") {
-            return (
-              <DesktopDropdown
-                key={item.id}
-                title={item.text}
-                categories={categories.productCategories}
-                basePath={item.url}
-                activeMenu={activeMenu}
-                onSetActiveMenu={setActiveMenu}
-              />
-            );
+            return renderProductTopLevelCategories();
           }
           if (item.url === "/blog") {
             return (
@@ -37,8 +38,6 @@ const DesktopNav = ({
                 title={item.text}
                 categories={categories.blogCategories}
                 basePath={item.url}
-                activeMenu={activeMenu}
-                onSetActiveMenu={setActiveMenu}
               />
             );
           }
