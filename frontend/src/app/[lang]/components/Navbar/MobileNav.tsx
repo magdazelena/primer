17830bd4752c291c39f7ffba3c30481488579c8b
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2024-09-30 14:39:33 +0200

    chore: full width mobile menu

--- frontend/src/app/[lang]/components/Navbar/MobileNav.tsx
+++ frontend/src/app/[lang]/components/Navbar/MobileNav.tsx
@@ -29,7 +29,7 @@ export default function MobileNav({
         }`}
       >
         <div
-          className={`fixed  w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-inset sm:ring-white/10 border-t-2 border-b-2 border-secondary text-lg `}
+          className={`fixed  w-full  overflow-y-auto bg-white px-6 py-6 sm:ring-1 sm:ring-inset sm:ring-white/10 border-t-2 border-b-2 border-secondary text-lg `}
         >
           <div className="space-y-2 ">
             {links.map((item: NavLink) => {
