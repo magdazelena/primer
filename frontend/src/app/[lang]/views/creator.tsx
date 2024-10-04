bb5ef51dcadffde6083447ade04c1561185613bb
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2024-10-04 17:35:14 +0200

    feat: creator grid

--- frontend/src/app/[lang]/views/creator.tsx
+++ frontend/src/app/[lang]/views/creator.tsx
@@ -1,7 +1,33 @@
 import { Creator } from "@/types/creator";
+import { getStrapiMedia } from "../utils/api-helpers";
+import Image from "next/image";
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 
 const CreatorView = ({ creator }: { creator: Creator }) => {
-  return <p>{creator.attributes.name}</p>;
+  const { avatar, name, bio } = creator.attributes;
+  const imageUrl = getStrapiMedia(avatar.data?.attributes.url);
+  return (
+    <article className="grid grid-cols-3 text-secondary">
+      <div className="col-span-12 lg:col-span-2">
+        <h1 className="leading-tight text-5xl font-bold ">{name}</h1>
+
+        <div className="text-secondary max-w-[1000px] rich-text">
+          <BlocksRenderer content={bio} />
+        </div>
+      </div>
+      <div className="col-span-12 lg:col-span-1 ">
+        {imageUrl && (
+          <Image
+            src={imageUrl}
+            alt="article cover image"
+            width={400}
+            height={400}
+            className="w-full h-96 object-cover rounded-lg"
+          />
+        )}
+      </div>
+    </article>
+  );
 };
 
 export { CreatorView };
