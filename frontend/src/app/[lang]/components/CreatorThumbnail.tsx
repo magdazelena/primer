26c6a8057afe8b5a6d19cecfd5c44cfeeb35f7fb
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2024-10-05 08:15:55 +0200

    chore: styling thumbnail

--- frontend/src/app/[lang]/components/CreatorThumbnail.tsx
+++ frontend/src/app/[lang]/components/CreatorThumbnail.tsx
@@ -7,7 +7,7 @@ const CreatorThumbnail = ({ creator }: { creator: Creator }) => {
   const { avatar, name, slug, lead } = creator.attributes;
   const authorImgUrl = getStrapiMedia(avatar.data.attributes.url);
   return (
-    <div className="flex items-center bg-accent2/30 p-10">
+    <div className="flex flex-wrap justify-end lg:justify-start items-center bg-accent2/30 p-10">
       {authorImgUrl && (
         <Image
           src={authorImgUrl}
@@ -17,7 +17,7 @@ const CreatorThumbnail = ({ creator }: { creator: Creator }) => {
           className="w-24 h-24 border rounded-full bg-accent2/50 border-accent2"
         />
       )}
-      <div className="pl-5 border-l-[2px] border-l-secondary ml-5">
+      <div className="pt-5 lg:pt-0 lg:pl-5 lg:border-l-[2px] lg:border-l-secondary lg:ml-5">
         <h5 className="text-lg font-bold text-secondary pb-5">{name}</h5>
         <p>{lead}</p>
         <Link href={`/creators/${slug}`}>Find out more about {name} here.</Link>
