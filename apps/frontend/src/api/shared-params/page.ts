88c8cb5d62e9092942bb1d7c43c698ad176b195f
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2025-01-31 14:17:25 +0100

    chore: fetch hero section

--- apps/frontend/src/api/shared-params/page.ts
+++ apps/frontend/src/api/shared-params/page.ts
@@ -1,46 +1,56 @@
 export const PAGE_CONTENT_SECTIONS_QUERY = {
-    contentSections: {
-      on: {
-        "sections.card-group": {
-          populate: {
-            cards: {
-              populate: "*",
-            },
+  contentSections: {
+    on: {
+      "sections.card-group": {
+        populate: {
+          cards: {
+            populate: "*",
           },
         },
       },
-    },
-  };
-
-  export const GLOBAL_LAYOUT_QUERY = {
-    populate: {
-          metadata: {
-            populate: "*",
+      "sections.hero": {
+        populate: {
+          picture: {
+            populate: "*"
           },
-          navbar: {
-            populate: {
-              navbarLogo: { populate: '*'},
-              menuItems: { populate: '*'}
-            },
-          },
-          footer: {
-            populate: {
-              'footerLogo': {
-                populate: '*'
-              },
-              menuLinks: {
-                populate: '*'
-              },
-              legalLinks: {
-                populate: '*'
-              },
-              socialLinks: {
-                populate: '*'
-              },
-              categories: {
-                populate: '*'
-              }
-            },
-          },
+          buttons: {
+            populate: '*'
+          }
         }
+      }
+    },
+  },
+};
+
+export const GLOBAL_LAYOUT_QUERY = {
+  populate: {
+    metadata: {
+      populate: "*",
+    },
+    navbar: {
+      populate: {
+        navbarLogo: { populate: '*' },
+        menuItems: { populate: '*' }
+      },
+    },
+    footer: {
+      populate: {
+        'footerLogo': {
+          populate: '*'
+        },
+        menuLinks: {
+          populate: '*'
+        },
+        legalLinks: {
+          populate: '*'
+        },
+        socialLinks: {
+          populate: '*'
+        },
+        categories: {
+          populate: '*'
+        }
+      },
+    },
   }
+}
