6dc2af053ac213f7bde5e793c922637a20f08522
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2025-01-31 15:28:57 +0100

    fix: find parent child categories in strapi 5 model

--- apps/backend/src/api/product-category/content-types/product-category/lifecycles.js
+++ apps/backend/src/api/product-category/content-types/product-category/lifecycles.js
@@ -4,7 +4,6 @@ module.exports = {
   // Hook to run after a category is created
   async afterCreate(event) {
     const { result } = event;
-
     // Call a function to handle updating parent with this new child
     await updateParentWithChild(result);
   },
@@ -19,29 +18,36 @@ module.exports = {
 };
 
 async function updateParentWithChild(category) {
-  if (category.parent) {
+  if (category.parent?.count > 0) {
     // Fetch the parent category
-    const parent = await strapi.entityService.findOne(
-      'api::product-category.product-category',
-      category.parent.id,
-      { populate: ['children'] } // Populate the children field
+    const categoryInfo = await strapi.entityService.findOne(
+      "api::product-category.product-category",
+      category.id,
+      { populate: ["parent"] }, // Populate the children field
     );
-
-    // If the parent exists and doesn't already contain this child
-    if (parent && !parent.children.some((child) => child.id === category.id)) {
-      // Add this category (child) to the parent's children field
-      await strapi.entityService.update(
-        'api::product-category.product-category',
-        parent.id,
-        {
-          data: {
-            children: [
-              ...parent.children.map((child) => child.id),
-              category.id,
-            ], // Append this child
-          },
-        }
-      );
-    }
+    let parent = categoryInfo?.parent;
+    if (!parent) return;
+    parent = await strapi.entityService.findOne(
+      "api::product-category.product-category",
+      parent.id,
+      { populate: ["children"] }, // Populate the children field
+    );
+    const addChildIfNone = (p, new_child) => {
+      const children = p.children.map((child) => child.id);
+      if (p.children.some((child) => child.id === category.id)) {
+        return children;
+      } else {
+        return [...children, new_child];
+      }
+    };
+    await strapi.entityService.update(
+      "api::product-category.product-category",
+      parent.id,
+      {
+        data: {
+          children: addChildIfNone(parent, category.id),
+        },
+      },
+    );
   }
 }
