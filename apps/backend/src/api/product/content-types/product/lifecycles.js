67932fc3e0db09a2fe7d8d9f87ad87b04a7e83ec
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2025-03-05 14:39:06 +0100

    chore: filter by status

---
+++ apps/backend/src/api/product/content-types/product/lifecycles.js
@@ -0,0 +1,19 @@
+module.exports = {
+    beforeFindOne(event) {
+        event.params.where = {
+            ...event.params.where,
+            statusName: {
+                published: true
+            }
+          };
+    },
+    beforeFindMany(event) {
+      event.params.where = {
+        ...event.params.where,
+        statusName: {
+            published: true
+        }
+      };
+    },
+  };
+  
