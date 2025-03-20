bb912740de8f6de36ab2d415ca1b1bdc10fd33bc
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2025-03-20 21:31:48 +0100

    chore: configure cypress

--- apps/frontend/cypress.config.ts
+++ apps/frontend/cypress.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'cypress';
-import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
 export default defineConfig({
   e2e: {
-    ...nxE2EPreset(__filename),
     baseUrl: 'http://localhost:3000',
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     video: false,
     screenshotOnRunFailure: true,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    experimentalStudio: true
   },
 }); 
