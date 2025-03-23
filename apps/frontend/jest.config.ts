ee4a77ac77a9c86e204483ff7dbcd2cfe80b2af4
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2025-03-23 09:56:11 +0100

    chore: add jest config

--- apps/frontend/jest.config.ts
+++ apps/frontend/jest.config.ts
@@ -1,36 +1,5 @@
-import 'ts-node/register';
-import type { Config } from 'jest';
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
-import { getJestProjectsAsync } from '@nx/jest';
-
-const config: Config = {
+export default {
   displayName: 'frontend',
-  projects: [],
-  preset: './jest-preset.json',
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
-  transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': ['ts-jest', {
-      tsconfig: '<rootDir>/tsconfig.json',
-    }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  preset: '../../jest.preset.js',
   coverageDirectory: '../../coverage/apps/frontend',
-  collectCoverageFrom: [
-    '**/*.{ts,tsx}',
-    '!**/*.stories.{ts,tsx}',
-    '!**/*.d.ts',
-    '!**/index.ts',
-    '!**/jest.setup.ts',
-  ],
 };
-async function setupConfig() {
-  const projects = await getJestProjectsAsync();
-  config.projects = projects;
-  return config;
-}
-export default setupConfig(); 
