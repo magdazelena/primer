e3f9749f1aeb843f5d9e1e33e292e559f1d67a99
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2025-07-12 13:06:09 +0200

    chore: fix lifecycles

--- apps/backend/src/api/product/content-types/product/lifecycles.ts
+++ apps/backend/src/api/product/content-types/product/lifecycles.ts
@@ -1,23 +1,24 @@
 import { generateUniqueSKU } from "./sku-generator";
-
+import { Lifecycle } from "@strapi/strapi";
+  
 export default {
-  async beforeCreate(event: any) {
+  async beforeCreate(event: Lifecycle.BeforeCreate) {
     const { data } = event.params;
 
     // Generate SKU if not provided
     if (!data.sku) {
-      data.sku = await generateUniqueSKU();
+      data.sku = await generateUniqueSKU(event.context.strapi);
     }
   },
 
-  async beforeCreateMany(event: any) {
+  async beforeCreateMany(event: Lifecycle.BeforeCreateMany) {
     const { data } = event.params;
 
     // Handle bulk creation
     if (Array.isArray(data)) {
       for (const item of data) {
         if (!item.sku) {
-          item.sku = await generateUniqueSKU();
+          item.sku = await generateUniqueSKU(event.context.strapi);
         }
       }
     }
