3df93074cf5af1c5d4abef18d3ec3ada3eb147cb
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2025-08-23 12:45:56 +0200

    chore: update frontend eslint config

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,30 +1,130 @@
-const {
-    defineConfig,
-} = require("eslint/config");
-
+const { defineConfig } = require("eslint/config");
 const js = require("@eslint/js");
+const tsParser = require("@typescript-eslint/parser");
+const react = require("eslint-plugin-react");
+const reactHooks = require("eslint-plugin-react-hooks");
+const nextPlugin = require("@next/eslint-plugin-next");
+const globals = require("globals");
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-module.exports = defineConfig([{
-    extends: compat.extends("eslint-config-primer/client", "plugin:@next/next/recommended"),
-
+module.exports = defineConfig([
+  // Base configuration with recommended rules
+  js.configs.recommended,
+  
+  // Base configuration
+  {
     languageOptions: {
-        parserOptions: {
-            project: "./tsconfig.json",
-            tsconfigRootDir: __dirname,
-        },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        ...globals.node,
+      },
     },
-
     rules: {
-        "@typescript-eslint/no-explicit-any": "off",
+      strict: ["error", "global"],
+      "no-return-await": "error",
+      "object-shorthand": [
+        "error",
+        "always",
+        {
+          avoidExplicitReturnArrows: true,
+        },
+      ],
+      "default-param-last": "warn",
+      "no-template-curly-in-string": "warn",
+      "no-console": "warn",
     },
-}]);
+  },
+  
+  // TypeScript files configuration
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: react,
+      "react-hooks": reactHooks,
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      // React rules
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/require-default-props": "off",
+      "react/function-component-definition": [2, {
+        namedComponents: "arrow-function",
+      }],
+      
+      // React Hooks rules
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      
+      // Next.js rules
+      "@next/next/no-html-link-for-pages": "error",
+      "@next/next/no-img-element": "warn",
+    },
+  },
+  
+  // JavaScript files configuration
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: react,
+      "react-hooks": reactHooks,
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/require-default-props": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "@next/next/no-html-link-for-pages": "error",
+      "@next/next/no-img-element": "warn",
+    },
+  },
+  
+  // Test files configuration
+  {
+    files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+  
+  // Global ignores
+  {
+    ignores: [
+      "**/node_modules",
+      "**/dist",
+      "**/build",
+      ".next/**/*",
+      "cypress/**/*",
+      "coverage/**/*",
+    ],
+  },
+]);
