9fade2f223fa7ca2e827c3d461b43fc33c6a9176
Magdalena Odrowąż-Żelezik <magda.zelezik@gmail.com>
2025-10-20 14:00:11 +0200

    chore: fix seeding (#498)
    * chore: fixed seeding
    
    * chore: fix filtering on list view

--- packages/strapi-plugin-status-manager/server/src/middlewares/filter-published.ts
+++ packages/strapi-plugin-status-manager/server/src/middlewares/filter-published.ts
@@ -1,34 +1,48 @@
-import { AnyDocument, Middleware as DocumentMiddleware } from "@strapi/types/dist/modules/documents";
+import {
+  AnyDocument,
+  Middleware as DocumentMiddleware,
+} from "@strapi/types/dist/modules/documents";
 
-export default async (context: DocumentMiddleware.Context, next: () => Promise<number | AnyDocument | AnyDocument[] | { documentId: string; entries: AnyDocument[]; }>) => {
-    const uid = context.uid;
-    if (!uid.includes('api::')) {
-      return next();
-    }
+export default async (
+  context: DocumentMiddleware.Context,
+  next: () => Promise<
+    | number
+    | AnyDocument
+    | AnyDocument[]
+    | { documentId: string; entries: AnyDocument[] }
+  >
+) => {
+  const uid = context.uid;
+  if (!uid.includes("api::")) {
+    return next();
+  }
 
-    const params = context.params;
-    if (!params && !params['status']) return next();
+  const params = context.params;
+  if ((!params && !params["status"]) || params["statusName"]) return next();
 
-     const unpublishedStatusLinks = await strapi.db.query('plugin::primer-status-manager.status-link').findMany({
-        populate: {
-          status: true
+  const unpublishedStatusLinks = await strapi.db
+    .query("plugin::primer-status-manager.status-link")
+    .findMany({
+      populate: {
+        status: true,
+      },
+      where: {
+        targetUid: uid,
+        status: {
+          published: false,
         },
-       where: { 
-         targetUid: uid,
-         status: {
-          published: false
-         }
-       },
-       select: ['targetDocumentId']
-     });
-     
-     const unpublishedDocumentIds = unpublishedStatusLinks.map(link => link.targetDocumentId);
-     
+      },
+      select: ["targetDocumentId"],
+    });
 
-      context.params['filters'] = {
-        ...context.params['filters'],
-        documentId: { $notIn: unpublishedDocumentIds }
-      };
+  const unpublishedDocumentIds = unpublishedStatusLinks.map(
+    (link) => link.targetDocumentId
+  );
 
-      return next();
-  } 
+  context.params["filters"] = {
+    ...context.params["filters"],
+    documentId: { $notIn: unpublishedDocumentIds },
+  };
+
+  return next();
+};
