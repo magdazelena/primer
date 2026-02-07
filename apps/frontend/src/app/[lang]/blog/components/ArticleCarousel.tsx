fc90adc685c76b9e38619ee57e8ca21bc071bc43
magdazelena <magda.zelezik@gmail.com>
2026-02-07 16:02:09 +0100

    chore: creators display

--- apps/frontend/src/app/[lang]/blog/components/ArticleCarousel.tsx
+++ apps/frontend/src/app/[lang]/blog/components/ArticleCarousel.tsx
@@ -2,10 +2,12 @@ import { ArticleThumbnail } from "./ArticleThumbnail";
 
 import type { Article } from "@/types/article";
 
-const ArticleCarousel = ({ articles }: { articles: Article[] }) => {
+const ArticleCarousel = ({ articles, title }: { articles: Article[], title?: string }) => {
   return (
     <div className="space-y-2">
-      <h4 className="text-lg font-semibold">Other Posts You May Like</h4>
+      <h4 className="text-lg font-semibold">
+        {title || "Posts You May Like"}
+      </h4>
       <div className="flex flex-wrap">
         {articles.map((article: Article) => {
           return <ArticleThumbnail key={article.id} article={article} />;
