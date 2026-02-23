1856b06fc97e8a406459dc68f18d1d85519336ed
magdazelena <magda.zelezik@gmail.com>
2026-02-23 14:53:36 +0100

    chore: modify product thumbnail

--- apps/frontend/src/app/[lang]/products/components/ProductThumbnail.tsx
+++ apps/frontend/src/app/[lang]/products/components/ProductThumbnail.tsx
@@ -13,7 +13,7 @@ const ProductThumbnail = ({ product }: { product: Product }) => {
     <Link
       href={`/products/${category?.slug}/${product.slug}`}
       key={product.id}
-      className="max-w-sm mb-3 lg:mr-8 group hover:no-underline focus:no-underline w-[250px] lg:w-[300px] xl:min-w-[375px] rounded-2xl overflow-hidden shadow-lg"
+      className="group hover:no-underline focus:no-underline rounded-lg overflow-hidden thumbnail-link p-2"
       data-testid="product-card"
     >
       {imageUrl && (
@@ -21,15 +21,17 @@ const ProductThumbnail = ({ product }: { product: Product }) => {
           alt="presentation"
           width="240"
           height="240"
-          className="object-cover w-full h-44 "
+          className="object-cover w-full h-100"
           src={imageUrl}
         />
       )}
-      <div className="p-6 space-y-2 relative text-dark">
+      <div className="pt-6 space-y-2 relative text-dark">
         <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
           {product.name}
         </h3>
-
+        {category?.name && (
+          <span className="label primary text-xs">{category.name}</span>
+        )}
         <div className="flex justify-between items-center">
           <h4 className="font-bold">{product.retailPrice.toFixed(2)}</h4>
         </div>
